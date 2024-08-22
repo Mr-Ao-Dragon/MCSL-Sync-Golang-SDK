fe282327a5986719074d43da5c9a31da9a1ae1e7
敖律风 <x123456789fy@outlook.com>
2024-08-22 21:53:57 +0800

    add: 添加读取版本列表

--- go.mod
+++ go.mod
@@ -2,12 +2,21 @@ module MCSL-Sync-Golang-SDK
 
 go 1.23.0
 
-require github.com/parnurzeal/gorequest v0.3.0
+require (
+	github.com/bytedance/sonic v1.12.1
+	github.com/parnurzeal/gorequest v0.3.0
+)
 
 require (
+	github.com/bytedance/sonic/loader v0.2.0 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/elazarl/goproxy v0.0.0-20240726154733-8b0c20506380 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/moul/http2curl v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/smartystreets/goconvey v1.8.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/net v0.28.0 // indirect
 )
--- info/list.go
+++ info/list.go
@@ -1,7 +1,37 @@
 package info
 
+import (
+	"MCSL-Sync-Golang-SDK/setup"
+	"errors"
+	"github.com/bytedance/sonic"
+	"github.com/parnurzeal/gorequest"
+)
+
 type ApiRepose struct {
 	Data []string `json:"data"`
 	Code int      `json:"code"`
 	Msg  string   `json:"msg"`
+}
+
+func (receiver *CoreList) add(name []string) {
+	*receiver = append(*receiver, name...)
+}
+func (receiver *CoreList) ReadCoreList(setupData setup.Client) (errs []error) {
+	request := gorequest.New()
+	_, body, httpErrs := request.Get(setupData.ApiDomain + "/core").End()
+	if httpErrs != nil {
+		errs = append(errs, httpErrs...)
+	}
+	var data ApiRepose
+	err := sonic.Unmarshal([]byte(body), &data)
+	if err != nil {
+		errs = append(errs, err)
+		return errs
+	}
+	if data.Code != 200 {
+		errs = append(errs, errors.New(data.Msg))
+		return
+	}
+	receiver.add(data.Data)
+	return nil
 }
