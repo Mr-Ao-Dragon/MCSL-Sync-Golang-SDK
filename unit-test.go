6e309846a06d5459cc48081e9a2522b808550f2b
敖律风 <x123456789fy@outlook.com>
2024-08-27 13:17:04 +0800

    fix: unit test target

---
+++ unit-test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/fs"
+	"log"
+	"os/exec"
+	"path/filepath"
+	"sync"
+)
+
+func main() {
+	dirPath := "./"
+	suffix := "_test.go"
+	var wg sync.WaitGroup
+	ch := make(chan string, 100)
+	errs := make(chan error, 100)
+	outputBytes := make([]byte, 100)
+	err := filepath.Walk(dirPath, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			errs <- err
+		}
+		if !info.IsDir() && filepath.Ext(path) == suffix {
+			wg.Add(1)
+			go func() {
+				cmd := exec.Command("go", "test", path)
+				outputBytes, err = cmd.CombinedOutput()
+				if err != nil {
+					errs <- err
+				}
+				ch <- string(outputBytes)
+			}()
+			defer wg.Done()
+		}
+		return nil
+	})
+	if err != nil {
+		errs <- err
+	}
+	if len(errs) != 0 {
+		log.Fatalf("Error: %#v", errs)
+	}
+	for Result := range ch {
+		log.Println(Result)
+	}
+}
