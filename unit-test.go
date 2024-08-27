85b290ebb279a44446a80756e27fde6c0cb43728
敖律风 <x123456789fy@outlook.com>
2024-08-27 19:02:45 +0800

    fix: 错误的异步代码位置 fix: 未关闭通道

--- unit-test.go
+++ unit-test.go
@@ -20,26 +20,35 @@ func main() {
 			errs <- err
 		}
 		if !info.IsDir() && filepath.Ext(path) == suffix {
-			wg.Add(1)
 			go func() {
+				wg.Add(1)
 				cmd := exec.Command("go", "test", path)
 				outputBytes, err = cmd.CombinedOutput()
 				if err != nil {
 					errs <- err
 				}
 				ch <- string(outputBytes)
+				defer wg.Done()
 			}()
-			defer wg.Done()
+
 		}
 		return nil
 	})
 	if err != nil {
 		errs <- err
 	}
+	go func() {
+		wg.Wait()
+	}()
 	if len(errs) != 0 {
-		log.Fatalf("Error: %#v", errs)
+		log.Fatalf("Error: %#v", <-errs)
 	}
 	for Result := range ch {
 		log.Println(Result)
 	}
+	defer func() {
+		close(ch)
+		close(errs)
+	}()
+	return
 }
